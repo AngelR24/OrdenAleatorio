fd9214434f56cdec671684fbfeefeca872565570
AngelR24 <49293458+AngelR24@users.noreply.github.com>
2022-02-27 15:34:59 -0400

    Random Numbres printed
    -Finished program, printing Random numbers up to N

--- RandomOrder/RandomOrder/Program.cs
+++ RandomOrder/RandomOrder/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace RandomOrder
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inserte un número N para imprimir números aleatorios.");
+            int n = Convert.ToInt32(Console.ReadLine());
+            while (n <= 0)
+            {
+                Console.WriteLine("El número debe ser mayor que 0");
+                Console.WriteLine("Intente otra vez:");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("**************************************");
+            printRandomNumbers(n);
+            Console.WriteLine("**************************************");
+            Console.WriteLine("Completado.");
+            Console.ReadKey();
+        }
+
+        private static void printRandomNumbers(int n)
+        {
+            var numbers = Enumerable.Range(1, n);
+            Random r = new Random();
+            var randomNumbers = numbers.OrderBy(x => r.Next()).Take(n);
+            foreach (var number in randomNumbers)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
